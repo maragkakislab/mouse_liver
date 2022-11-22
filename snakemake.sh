27c7a581b85fdf48b170fda3053a9e0601061567
Abid Rehman <abid.rehman.de@gmail.com>
2022-11-22 11:10:48 -0500

    changes per comments made

--- snakemake.sh
+++ snakemake.sh
@@ -4,5 +4,4 @@ module load snakemake || exit 1
 
 source myconda
 mamba activate base
-#snakemake -pr --keep-going --retries 3 --latency-wait 120 --use-conda --use-envmodules -s workflow/Snakefile --profile workflow/snakemake_profile
-snakemake -pr --keep-going --latency-wait 120 --use-envmodules -s workflow/Snakefile --profile workflow/snakemake_profile #only use env modules for now
+snakemake -pr --keep-going --retries 3 --latency-wait 120 --use-conda --use-envmodules -s workflow/Snakefile --profile workflow/snakemake_profile #correction made
